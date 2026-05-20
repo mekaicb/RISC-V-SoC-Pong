80ed50daaa79ea5892762c62d45a8564c0d3605e
Mekai <root@DESKTOP-M2F8409.localdomain>
2026-05-20 16:23:13 -0400

    Reduced ROM size from 270kB to 64kB

--- hdl/instruction_memory.sv
+++ hdl/instruction_memory.sv
@@ -4,22 +4,17 @@ module instruction_memory(
 	output logic [31:0] data_out
 	);
 	
-	logic [31:0] mem_array[0:69119]; //69120 words in a single column
+	logic [31:0] mem_array[0:16383]; // 64kB/270kB dedicated to ROM. 64kB = 16384 words in one column
 	
-	// Note: Wont compile since quartus will try to load 32x69120 dff's. Will run in Modelsim though
-
-	initial $readmemh("../test/sumtest.txt", mem_array); //read data from hex_file.txt and write to mem_array
+	initial $readmemh("../test/sumtest.hex", mem_array); //read data from hex_file.txt and write to mem_array
 		
 	always_ff @(posedge clk) begin
-		data_out <= mem_array[addr_in[18:2]]; // 17 address bits to select one word (log(69120) = 16.02 -> 17)
+		data_out <= mem_array[addr_in[15:2]]; // 5 address bits to select one word (log(16384) = 14)
 	end
 	
 endmodule
 
 
 /*
-Without shifting: addr_in = 0x00000004 -> mem_array[4] -> 5th word
-But we want this to refer to byte 5, not word 5. 
-To convert this to byte addressibility, divide by 4 (shift bits right by two)
-Which is the same as addressing [18:2] instead of [16:0]
+[15:2] forces word addressibility. Only addresses that're multiples of 4 are addressible in ROM. 
 */
