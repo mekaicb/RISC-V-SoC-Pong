dadd7dc9e7cb91c4a64617886ba3c2b0659e37d3
Mekai <root@DESKTOP-M2F8409.localdomain>
2026-05-20 18:44:03 -0400

    Added MEM/WB buffer

---
+++ hdl/MEMWB_buffer.sv
@@ -0,0 +1,25 @@
+module MEMWB_buffer(
+	input logic regwrite_i, memtoreg_i, clk, rst_n,
+	input logic [31:0] memdata_i,
+	input logic [31:0] ALU_result_i,
+	output logic regwrite_o, memtoreg_o,
+	output logic [31:0] memdata_o,
+	output logic [31:0] ALU_result_o
+	);
+	
+	always_ff @(posedge clk) begin
+		if(!rst_n) begin	
+			regwrite_o <= 1'b0;
+			memtoreg_o <= 2'b0;
+			memdata_o <= 32'b0;
+			ALU_result_o <= 32'b0;
+		end
+		else begin
+			regwrite_o <= regwrite_i;
+			memtoreg_o <= memtoreg_i;
+			memdata_o <= memdata_i;
+			ALU_result_o <= ALU_result_i;
+		end
+	end
+endmodule
+	
