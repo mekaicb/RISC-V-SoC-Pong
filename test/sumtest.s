edc9e792a5a411a9d2e83392045609af35cc9174
Mekai <root@DESKTOP-M2F8409.localdomain>
2026-05-27 13:55:49 -0400

    Fixed stack pointer initialization

--- test/sumtest.s
+++ test/sumtest.s
@@ -1,12 +1,12 @@
-# Assembly to test ID/EX execution. Code increments through a list in memory and computes the total sum.
+# Assembly to test CPU functionality. Code increments through a list in memory and computes the total sum.
 
 .text
 .global _start
 
 _start:
-	li 	sp, 0x43800	# li = load immediate, pretty much the same as la except assembler is not providing the value (you are)
+	li 	sp, 0x30000		# li = load immediate. 0x30000 = RAM end (64kb (ROM) + 128kb (RAM))
 	call	main
-	jal	x0, 0		# Infinite loop: Jumps to PC + 0	
+	jal	x0, 0				# Infinite loop: Jumps to PC + 0	
 
 main:
 	la		t0, N           # t0 = address of N (la = movia equivalent)
@@ -23,17 +23,10 @@ LOOP:
 	la		t0, SUM         # t1 = address of SUM
 	sw 	t3, 0(t0)       # store sum
 	ret
-	
+
 .data
 SUM:		.space 4
 N:  		.word  5
-LIST: 	.word  12, 0xFFFFFFFE, 7, -1, 2
+LIST: 	.word  1, 2, 3, 4, 5
 
 # .end not required for RISC-V
-
-# Note
-# li is a pseudoinstruction. It expects a 32 bit immediate. So to keep the rule of 32 bit instructions, it splits the 32 bit imm into the upper 20 and lower 12 bits. Then it does lui on the upper 20, and adds the lower 12 bits. For ex, 0x00043800 -> 0x00043 & 0x800.
-# Numbers get turned into 0x00044 and 0xFFFFF800 by assembler magic
-# lui sp, 0x44
-# addi sp, sp, 0xFFFFF800
-# Explains why modelsim waveform may show unexpected imm_o/opcode
