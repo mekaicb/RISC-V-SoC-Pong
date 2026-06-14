cf4d57bae4ec05c57416c231f9ab6eac3605579e
Mekai <mekai.bingham@gmail.com>
2026-06-13 20:44:29 -0400

    Adjusted memory for DE-10 Lite instead of DueProLogic, increased size for rgb support

--- VGA/frame_buffer.sv
+++ VGA/frame_buffer.sv
@@ -1,17 +1,21 @@
 module frame_buffer(
 	input logic clk, we,
-	input logic [13:0] addr_w, addr_r,
+	input logic [14:0] addr_w, addr_r,
 	input logic [31:0] data_i,
 	output logic [31:0] data_o
 	);
 	
-	logic [31:0] vram[0:9599]; 
+	logic [14:0] eff_addr_r, eff_addr_w;
+	(* ramstyle = "M9K" *) logic [31:0] vram[0:19199]; 
+	localparam offset = 32'h00028000;
+	
+	assign eff_addr_r = addr_r - offset;
+	assign eff_addr_w = addr_w - offset;
 	
 	always_ff @(posedge clk) begin 
 		if(we)
-			vram[addr_w] <= data_i;// Synchronous write
-			
-		data_o = vram[addr_r[13:0]]; // Asynchronous read
+			vram[eff_addr_w] <= data_i;// Synchronous write
+		data_o <= vram[eff_addr_r]; // Synchronous read
 	end	
 	
 endmodule
