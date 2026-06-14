ee66f4149c223bd66463f888a642738342ed8c5e
Mekai <mekai.bingham@gmail.com>
2026-06-14 12:49:47 -0400

    Clock fixes

--- VGA/VGA.sv
+++ VGA/VGA.sv
@@ -1,5 +1,5 @@
 module VGA(
-	input logic clock, rst_n, we,
+	input logic clk_50, rst_n, we, // clk_50 is the 50MHz clock input 
 	input logic [31:0] data_i,
 	input logic [17:0] addr_i,
 	output logic [2:0] rgb,
@@ -11,12 +11,12 @@ module VGA(
 	logic [3:0] pixel_sel;
 	logic [31:0] data_o;
 	logic video_on;
-	logic clk;
+	logic clk; // 25.175MHz input driving VGA modules
 	
 	assign pixel_sel = x[3:0]; // The upper bits of x are used to select the word, the bits shifted out are the "remainder" and are used to select the pixel in the word
 	
-	PLL PLL(.inclk0(clock), .c0(clk));
-	frame_buffer frame_buffer(.clk(clk), .we(we), .addr_w(addr_i), .data_i(data_i), .addr_r(addr_r),.data_o(data_o));
+	PLL PLL(.inclk0(clk_50), .c0(clk)); // Generates the 25.175MHz clock frequency
+	frame_buffer frame_buffer(.clk_50(clk_50), .clk_25(clk), .we(we), .addr_w(addr_i), .data_i(data_i), .addr_r(addr_r),.data_o(data_o));
 	VGA_controller VGA_controller(.clk(clk), .rst_n(rst_n), .x(x), .y(y), .h_sync(hsync), .v_sync(vsync), .video_on(video_on));
 	pixel_color pixel_color(.clk(clk), .pixel_sel(pixel_sel), .video_on(video_on), .data_o(data_o), .rgb(rgb));
 	read_pipe read_pipe(.x(x), .y(y), .r_addr(addr_r));
--- VGA/frame_buffer.sv
+++ VGA/frame_buffer.sv
@@ -1,5 +1,5 @@
 module frame_buffer(
-	input logic clk, we,
+	input logic clk_50, clk_25, we,
 	input logic [17:0] addr_w,
 	input logic [14:0] addr_r,
 	input logic [31:0] data_i,
@@ -9,6 +9,7 @@ module frame_buffer(
 	logic [17:0] eff_addr_w;
 	logic [14:0] eff_addr_r;
 	
+	
 	(* ramstyle = "M9K" *) logic [31:0] vram[0:19199]; 
 	localparam offset = 32'h00028000; 
 	
@@ -17,10 +18,13 @@ module frame_buffer(
 	assign eff_addr_r = addr_r; // address from read pipe is already from 0 to 19199, no need to subtract offset
 	assign eff_addr_w = addr_w - offset; // address from processor would be something like 0x00028000, need to subtract offset
 	
-	always_ff @(posedge clk) begin 
+	always_ff @(posedge clk_50) begin // Writes at processor speed, 50MHz
 		if(we)
 			vram[eff_addr_w] <= data_i;// Synchronous write
-		data_o <= vram[eff_addr_r]; // Synchronous read
 	end	
+
+	always_ff @(posedge clk_25) begin // Reads at 25.175MHz
+		data_o <= vram[eff_addr_r]; // Synchronous read
+	end
 	
 endmodule
