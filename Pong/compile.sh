f1cb874c0237002a761e4cf63bccc4cde38fe8a4
Mekai <mekai.bingham@gmail.com>
2026-06-26 21:16:20 -0400

    Added libgcc for %, *, / operations

--- Pong/compile.sh
+++ Pong/compile.sh
@@ -1,5 +1,5 @@
 riscv64-unknown-elf-gcc -march=rv32i -mabi=ilp32 -nostdlib -c pong.c -o pong.o
-riscv64-unknown-elf-ld -m elf32lriscv -T linker2.ld pong.o -o pong.elf
+riscv64-unknown-elf-ld -m elf32lriscv -T link.ld pong.o /usr/lib/gcc/riscv64-unknown-elf/13.2.0/rv32i/ilp32/libgcc.a -o pong.elf
 riscv64-unknown-elf-objcopy -O verilog pong.elf pong.hex
 python3 makehex.py
 cp pong.hex ../CPU/synthesis
